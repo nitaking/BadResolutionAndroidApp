601c8a7eee7351bb2afaf92476dade0b118d9814
nitaking <10850034+nitaking@users.noreply.github.com>
2019-12-16 02:12:31 +0900

    commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,101 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Carousel from "react-native-snap-carousel";
+import React, {PureComponent} from "react";
+import {Dimensions, Text, View, StyleSheet, TouchableOpacity} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This is BadResolutionAndroidApp.</Text>
-    </View>
-  );
+const itemWidth = 100;
+const styles = StyleSheet.create({
+    pageNation: {
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    },
+    dot: {
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        marginHorizontal: 8,
+        backgroundColor: 'white',
+    },
+    card: {
+        width: itemWidth,
+        height: 150,
+        backgroundColor: "green",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    button: {
+        backgroundColor: 'white',
+        paddingHorizontal: 20,
+        paddingVertical: 8,
+        borderRadius: 6,
+        marginTop: 30
+    },
+    activeIndex: {
+        color: "white",
+        fontSize: 20,
+        fontWeight: "bold",
+    }
+});
+
+let out = Array.from(Array(10), (_,x) => x);
+const data = out.map(_ => ({ status: false }));
+
+function sleep(ms: number) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Card = ({item, index, onNext}) => {
+    return (
+        <View style={styles.card}>
+            <Text style={styles.activeIndex}>{`${index}`}</Text>
+            <TouchableOpacity onPress={onNext} style={styles.button}>
+                <Text>次へ</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const Container = () => {
+    const firstItem = 1;
+    const [activeIndex, changeSlide] = React.useState(firstItem)
+    const carouselRef = React.useRef(null)
+
+    const onNext = React.useCallback( async () => {
+        const lastIndex = data.length - 1
+        const nextIndex = lastIndex ? activeIndex : activeIndex + 1;
+        const carousel = carouselRef.current
+
+        await sleep(10)
+        carousel.snapToItem(activeIndex + 1)
+        // setTimeout(() => carousel.snapToItem(nextIndex), 10)
+
+        changeSlide(nextIndex)
+    }, [activeIndex])
+
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Carousel
+                ref={carouselRef}
+                activeSlideAlignment={"center"}
+                containerCustomStyle={{paddingLeft: 0, backgroundColor: '#dbffff', paddingTop: 200}}
+                data={data}
+                itemWidth={itemWidth}
+                onSnapToItem={changeSlide}
+                renderItem={({item}) => <Card item={item} index={activeIndex} onNext={onNext} />}
+                sliderWidth={Dimensions.get("window").width}
+                firstItem={firstItem}
+            />
+            <Text style={{color: "red"}}>{`Active index: ${activeIndex}`}</Text>
+        </View>
+    )
+}
+
+export default class App extends PureComponent {
+    render() {
+        return (
+            <Container />
+        );
+    }
+}
